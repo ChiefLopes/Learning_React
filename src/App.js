7c6779d233cbc9e6c0848bccc22ee20dce270616
ChiefLopes <chieflopesekede@gmail.com>
2023-09-21 14:58:35 +0100

    used the useState hook to chane the state of three buttons, one increases, second decreases and the third resets to the initial state being zero

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+// import Planets from "./components/Planets";
+import { useState } from "react";
+
+function App() {
+  const age = 100;
+  const isGreen = false;
+
+  const [count, setCount] = useState(0);
+  const [textColor, setTextColor] = useState("black");
+
+  const names = [
+    { name: "Peter", age: 20 },
+    { name: "John", age: 30 },
+    { name: "Mary", age: 40 },
+  ];
+  // example of using conditional rendering
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Saturn", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
+    { name: "Neptune", isGasPlanet: true },
+  ];
+
+  return (
+    <>
+      <div className="App">
+        <h1 className="Name">{age <= 18 ? "age" : "over"}</h1>
+        <div>
+          <h1 style={{ color: isGreen ? "green" : "red" }}> Has the color</h1>
+        </div>
+        {names.map((user) => (
+          <div>
+            {user.name} {user.age}
+          </div>
+        ))}
+
+        {/* {planets.map(
+          (planet, key) => planet.isGasPlanet && <h1>{planet.name}</h1>
+        )} */}
+
+        <div>
+          <h1>{count}</h1>
+          <button
+            style={{ height: "30px", width: "50px" }}
+            onClick={() => setCount(count + 1)}>
+            +
+          </button>
+          <button
+            style={{ height: "30px", width: "50px" }}
+            onClick={() => setCount(count - 1)}>
+            -
+                  </button>
+                  <button onClick={() => setCount(0)}>Reset</button>
+
+          {<h1 style={{ color: textColor }}>Lopes 4 life</h1>}
+
+          <button
+            style={{ height: "30px", width: "50px" }}
+            onClick={() => {
+              setTextColor(textColor === "black" ? "red" : "black");
+            }}></button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Planets.jsx
@@ -0,0 +1,16 @@
+
+const Planets = () => {
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Saturn", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
+    { name: "Neptune", isGasPlanet: true },
+  ];
+
+  return planets;
+};
+
+export default Planets;
